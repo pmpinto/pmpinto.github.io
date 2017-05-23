d3efe3b0551bb7f424db731d3643f48762707527
Pedro Pinto <pedropinto.web@gmail.com>
2017-05-23 22:39:11 +0100

    Launch 2017 version

--- development/assets/js/main.js
+++ development/assets/js/main.js
@@ -1,3 +1,43 @@
-var utils = require('./utils');
+(() => {
+    // SETUP
+    let now = new Date();
 
-utils.log('Testing 123...');
+    this.elements = {
+        body: document.querySelector('body'),
+        logo: document.querySelector('.logo'),
+        toggles: document.getElementsByClassName('js-toggleNightMode')
+    };
+
+
+    // FUNCTIONS
+    const toggleNightMode = (element) => {
+        if (element.classList) { // <- confirm if this is needed on caniuse.com
+            if (element.classList.contains('night-mode')) {
+                element.classList.remove('night-mode');
+            } else {
+                element.classList.add('night-mode');
+            }
+        }
+    };
+
+
+    // EVENTS
+    this.elements.logo.addEventListener('dblclick', () => {
+        toggleNightMode(this.elements.body);
+    }, false);
+
+    // for (let toggle of this.elements.toggles) { <- find out why this is not working in Safari
+    for (var i = this.elements.toggles.length - 1; i >= 0; i--) {
+        this.elements.toggles[i].addEventListener('click', (event) => {
+            event.preventDefault();
+            toggleNightMode(this.elements.body);
+        }, false);
+    }
+
+
+    // RUNTIME
+    if (now.getHours() >= 20 || now.getHours() <= 7) {
+        toggleNightMode(this.elements.body);
+    }
+
+})();
